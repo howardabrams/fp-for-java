2a3189306910034a0de7383e3df5cc75d12fbd54
Howard Abrams <howard.abrams@gmail.com>
2012-10-13 00:10:47 -0700

    Better closure examples by building a 'sum'.

--- src/test/java/org/howardism/fpjava/ClosureTests.java
+++ src/test/java/org/howardism/fpjava/ClosureTests.java
@@ -21,6 +21,8 @@ package org.howardism.fpjava;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 /**
@@ -35,6 +37,8 @@ public class ClosureTests {
     final Closure theTruth   = new TheTruth();
     final Closure conjecture = new Conjecture();
 
+    final Closure sum = new Sum();
+
     @Test
     public void testBooleans() {
         assertTrue( (Boolean) theTruth.apply() );
@@ -46,9 +50,60 @@ public class ClosureTests {
             return Boolean.TRUE;
         }
     }
+
     class Conjecture implements Closure {
         public Object apply(Object... objects) {
             return Boolean.FALSE;
+        }
+    }
+
+    // ------------------------------------------------------------- //
+
+    /**
+     * The most standard closure is a <code>sum</code> function that acts
+     * recursively on an arbitrary number of arguments.
+     */
+    @Test
+    public void testSum() {
+        assertEquals(10, new Closure() {
+            public Object apply(Object... objects) {
+                int total = 0;
+                for (Object n : Arrays.asList(objects)) {
+                    total += (Integer) n;
+                }
+                return total;
+            }
+        }.apply(1, 2, 3, 4));
+    }
+
+    /**
+     * Could there be anything more obtuse and ugly than the previous code?
+     * Let's see if breaking it out into bite-size morsels helps us out any.
+     */
+    @Test
+    public void testSumAgain() {
+        assertEquals(10, sum.apply(1,2,3,4));
+    }
+
+    class Sum extends RecursiveClosure {
+        public Object apply(Object ... objects) {
+            if (objects.length == 0)
+                return 0;
+            return (Integer) head(objects) +
+                   (Integer) apply( tail(objects) );
+        }
+    }
+
+    /**
+     * To make a recursive call dealing with variable length parameter lists,
+     * we create this class that has a head and a tail function.
+     */
+    abstract class RecursiveClosure implements Closure {
+        <T> T head(T ... o) {
+            return o[0];
+        }
+        <T> T[] tail(T ... o) {
+            return Arrays.copyOfRange(o, 1, o.length-1);
         }
     }
 }
